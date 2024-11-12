550413a7f286c55524455fc895daac625d07ad91
Josie Cialdini <jcialdini@my.wctc.edu>
2024-11-12 13:18:24 -0600

    [1.10] Display categories

--- Program.cs
+++ Program.cs
@@ -2,10 +2,14 @@
 using NLog;
 using System.Linq;
 using NorthwindConsole.Model;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 string path = Directory.GetCurrentDirectory() + "//nlog.config";
+
 // create instance of Logger
 
 var logger = LogManager.Setup().LoadConfigurationFromFile(path).GetCurrentClassLogger();
+
 logger.Info("Program started");
 
 do
@@ -16,9 +20,26 @@ do
   string? choice = Console.ReadLine();
   Console.Clear();
   logger.Info("Option {choice} selected", choice);
+
   if (choice == "1")
   {
     // display categories
+    var configuration = new ConfigurationBuilder()
+            .AddJsonFile($"appsettings.json");
+
+    var config = configuration.Build();
+
+    var db = new DataContext();
+    var query = db.Categories.OrderBy(p => p.CategoryName);
+
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"{query.Count()} records returned");
+    Console.ForegroundColor = ConsoleColor.Magenta;
+    foreach (var item in query)
+    {
+      Console.WriteLine($"{item.CategoryName} - {item.Description}");
+    }
+    Console.ForegroundColor = ConsoleColor.White;
   }
   else if (choice == "2")
   {
